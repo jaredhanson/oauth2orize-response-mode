a51ea97eb28273977263bcc58e7b0bc3abf14a39
Jared Hanson <jaredhanson@gmail.com>
2015-10-30 19:33:58 -0700

    Initial commit.

---
+++ test/package.test.js
@@ -0,0 +1,11 @@
+/* global describe, it, expect */
+
+var pkg = require('..');
+
+describe('oauth2orize-rm', function() {
+  
+  it('should export object', function() {
+    expect(pkg).to.be.an('object');
+  });
+  
+});
